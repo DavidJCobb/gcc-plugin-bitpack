07ce473d31d2500704da7acf78aac1edad130b96
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2024-12-17 02:46:19 -0500

    Update setup.sh
    Added the ability to force-clean the GCC prerequisites, in case a download corrupts due to a spotty Internet connection.
    
    Added an option to literally just do the GMP setup, since apparently, directory checks in bash via the -d syntax are just fucking broken. There are half a dozen other syntaxes for probing for directories, but I'm done trial-and-erroring this bullshit tonight. I genuinely do not understand how this language is even more broken than CMD.EXE shell scripting.

--- setup.sh
+++ setup.sh
@@ -31,11 +31,15 @@ usage() {
    echo ""
    echo "Additional options:"
    echo " -f, --fast                      fast build (w/o localization, bootstrapping, etc.)"
+   echo " --erase-prerequisites           clean prerequisites first (e.g. to fix corrupt downloads)"
+   echo " --just-fix-gmp                  get GMP in the build directory and do nothing else"
    exit 1;
 }
 
 version=
 fast=0
+erase_prerequisites=0
+just_fix_gmp=0
 extract_args() {
    #
    # Check for GNU's variant on getopt, which properly supports long option 
@@ -70,7 +74,7 @@ extract_args() {
       # for the lack of separators and the fact that all names are only 
       # one character long.
       #
-      parsed="$(getopt -a -l 'version:,fast' -o 'v:f' -- $@)"
+      parsed="$(getopt -a -l 'version:,fast,erase-prerequisites,just-fix-gmp' -o 'v:f' -- $@)"
       if [ $? != "0" ]; then
          echo "bad"
          usage
@@ -86,6 +90,14 @@ extract_args() {
                ;;
             "-f"|"--fast")
                fast=1
+               shift
+               ;;
+            "--erase-prerequisites")
+               erase_prerequisites=1
+               shift
+               ;;
+            "--just-fix-gmp")
+               just_fix_gmp=1
                shift
                ;;
             # Boilerplate:
@@ -186,140 +198,120 @@ set_window_title() {
 
 cd $BASEDIR/dl
 
-download_archive() {
-   set_window_title "Downloading GCC ${version}..."
-   report_step "Downloading GCC ${version} to <h>$HOME/gcc/dl/</h>..."
-   echo "Downloading from: http://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz"
-   echo "";
+deal_with_archive() {
+   download_archive() {
+      set_window_title "Downloading GCC ${version}..."
+      report_step "Downloading GCC ${version} to <h>$HOME/gcc/dl/</h>..."
+      echo "Downloading from: http://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz"
+      echo "";
 
-   wget "http://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz" --continue
-   result=$?
-   if [ $result == 1 ]; then
-      report_error_and_exit "Download failed. Aborting installation process."
-   elif [ $result == 3 ]; then
-      report_error_and_exit "Download failed. (File I/O issue.) Aborting installation process."
-   elif [ $result == 4 ]; then
-      report_error_and_exit "Download failed. (Network failure.) Aborting installation process."
-   elif [ $result == 5 ]; then
-      report_error_and_exit "Download failed. (SSL verification failure.) Aborting installation process."
-   elif [ $result == 8 ]; then
-      report_error_and_exit "Download failed. (Server issued an error response; missing file?) Aborting installation process."
-   elif [ $result != 0 ]; then
-      report_error_and_exit "Download failed. Aborting installation process."
-   fi
-}
-unpack_archive() {
-   local extra_params=
-   if [ -z "$1" ]; then
-      extra_params="${extra_params} --skip-old-files"
-   fi
-
-   set_window_title "Unpacking GCC ${version}..."
-   report_step "Unpacking GCC ${version} to <h>$HOME/gcc/dl/gcc-${version}/</h>..."
-   echo ""
-   
-   result=0
-   if [[ $pv_available != 0 ]]; then
-      #
-      # The `pv` command is not installed. Extract files without a 
-      # progress indicator.
-      #
-      tar xf "gcc-${version}.tar.gz" $extra_params --checkpoint-action=ttyout='(%d seconds elapsed): %T%*\r'
+      wget "http://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz" --continue
       result=$?
-   else
-      #
-      # If the `pv` command is available, then use it to show a 
-      # progress bar for the extraction. In this case, `pv` is 
-      # able to show progress because it's responsible for feeding 
-      # the input file into `tar`, and it knows how much it's fed.
-      #
-      pv "gcc-${version}.tar.gz" | tar x --skip-old-files
-      result=${PIPESTATUS[1]}
-   fi
-   return $result;
-}
-
-already_have_archive=0
-if [ -f "gcc-${version}.tar.gz" ]; then
-   report_step "You already have the archive for GCC ${version} downloaded to <h>$HOME/gcc/dl/gcc-${version}.tar.gz</h>; skipping download."
-   already_have_archive=1
-else
-   download_archive;
-fi
-
-unpack_archive;
-if (( $? != 0 )); then
-   if (( $already_have_archive != 0 )); then
-      #
-      # The user already had the archive file, so we didn't bother 
-      # downloading it. If it was an interrupted download, though, 
-      # then that will have been a mistake.
-      #
-      report_error "Unpacking failed. Perhaps the download was interrupted? Re-downloading..."
-      
-      download_archive;
-      unpack_archive 'dont_skip_old_files';
-      if (( $? != 0 )); then
-         report_error_and_exit "Unpacking failed again. Aborting installation process."
+      if [ $result == 1 ]; then
+         report_error_and_exit "Download failed. Aborting installation process."
+      elif [ $result == 3 ]; then
+         report_error_and_exit "Download failed. (File I/O issue.) Aborting installation process."
+      elif [ $result == 4 ]; then
+         report_error_and_exit "Download failed. (Network failure.) Aborting installation process."
+      elif [ $result == 5 ]; then
+         report_error_and_exit "Download failed. (SSL verification failure.) Aborting installation process."
+      elif [ $result == 8 ]; then
+         report_error_and_exit "Download failed. (Server issued an error response; missing file?) Aborting installation process."
+      elif [ $result != 0 ]; then
+         report_error_and_exit "Download failed. Aborting installation process."
       fi
+   }
+   unpack_archive() {
+      local extra_params=
+      if [ -z "$1" ]; then
+         extra_params="${extra_params} --skip-old-files"
+      fi
+
+      set_window_title "Unpacking GCC ${version}..."
+      report_step "Unpacking GCC ${version} to <h>$HOME/gcc/dl/gcc-${version}/</h>..."
+      echo ""
+      
+      result=0
+      if [[ $pv_available != 0 ]]; then
+         #
+         # The `pv` command is not installed. Extract files without a 
+         # progress indicator.
+         #
+         tar xf "gcc-${version}.tar.gz" $extra_params --checkpoint-action=ttyout='(%d seconds elapsed): %T%*\r'
+         result=$?
+      else
+         #
+         # If the `pv` command is available, then use it to show a 
+         # progress bar for the extraction. In this case, `pv` is 
+         # able to show progress because it's responsible for feeding 
+         # the input file into `tar`, and it knows how much it's fed.
+         #
+         pv "gcc-${version}.tar.gz" | tar x --skip-old-files
+         result=${PIPESTATUS[1]}
+      fi
+      return $result;
+   }
+
+   local already_have_archive=0
+   if [ -f "gcc-${version}.tar.gz" ]; then
+      report_step "You already have the archive for GCC ${version} downloaded to <h>$HOME/gcc/dl/gcc-${version}.tar.gz</h>; skipping download."
+      already_have_archive=1
    else
-      report_error_and_exit "Unpacking failed. Aborting installation process."
+      download_archive;
    fi
+
+   unpack_archive;
+   if (( $? != 0 )); then
+      if (( $already_have_archive != 0 )); then
+         #
+         # The user already had the archive file, so we didn't bother 
+         # downloading it. If it was an interrupted download, though, 
+         # then that will have been a mistake.
+         #
+         report_error "Unpacking failed. Perhaps the download was interrupted? Re-downloading..."
+         
+         download_archive;
+         unpack_archive 'dont_skip_old_files';
+         if (( $? != 0 )); then
+            report_error_and_exit "Unpacking failed again. Aborting installation process."
+         fi
+      else
+         report_error_and_exit "Unpacking failed. Aborting installation process."
+      fi
+   fi
+}
+if [ $just_fix_gmp == 0 ]; then
+   deal_with_archive;
 fi
 
-set_window_title "Downloading prerequisites (in-tree) for GCC ${version}..."
-report_step "Downloading prerequisites (in-tree) for GCC ${version}..."
-echo ""
-
-cd "gcc-${version}"
-./contrib/download_prerequisites
-
-mkdir -p $BASEDIR/build/$version
-mkdir -p $BASEDIR/install/$version
-
-set_window_title "Building GCC ${version}..."
-report_step "Building GCC ${version} to <h>$HOME/gcc/build/${version}/</h>...";
-echo "";
-
-cd build/$version/
-#
-# Start by configuring the build process. This step will create the makefiles 
-# that we'll use to actually build GCC. You could think of it as building the 
-# makefiles.
-#
-../../dl/gcc-$version/configure --prefix=$BASEDIR/install/$version/ $GCC_CONFIG
-#
-# Run the makefiles to build GCC.
-#
-make -j$(nproc)
-if [ $? != 0 ]; then
-   report_error_and_exit "Build failed. Aborting installation process."
-fi
-#
-set_window_title "Installing GCC ${version}..."
-report_step "Installing GCC version ${version} to <h>$HOME/gcc/install/${version}/</h>..."
-echo ""
-
-make install
-if [ $? != 0 ]; then
-   report_error_and_exit "Installation failed. Aborting installation process."
+deal_with_prereqs() {
+   cd "${BASEDIR}/dl/gcc-${version}"
+   if [ $erase_prerequisites != 0 ]; then
+      set_window_title "Cleaning out prerequisites (in-tree) for GCC ${version}..."
+      report_step "Cleaning out prerequisites (in-tree) for GCC ${version}..."
+      echo ""
+      
+      find . -name 'gettext-*.tar.*' -exec rm {} \;
+      find . -name 'gmp-*.tar.*' -exec rm {} \;
+      find . -name 'isl-*.tar.*' -exec rm {} \;
+      find . -name 'mpc-*.tar.*' -exec rm {} \;
+      find . -name 'mpfr-*.tar.*' -exec rm {} \;
+   fi
+   #
+   set_window_title "Downloading prerequisites (in-tree) for GCC ${version}..."
+   report_step "Downloading prerequisites (in-tree) for GCC ${version}..."
+   echo ""
+   #
+   ./contrib/download_prerequisites
+   if (( $? != 0 )); then
+      report_error_and_exit "Failed to download prerequisites. Aborting installation process. Recommend retrying with --erase-prerequisites to clear any corrupt downloads."
+   fi
+}
+if [ $just_fix_gmp == 0 ]; then
+   deal_with_prereqs;
 fi
 
-if [ $fast == 1 ]; then
-   #
-   # When you perform a bootstrapping build of GCC (the default, I believe), 
-   # it will install GMP headers to `objdir/gmp`, i.e. `build/$version/gmp`. 
-   # However, the --disable-bootstrap config option will prevent this from 
-   # happening.
-   #
-   # Compiling a GCC plug-in requires having the GMP headers available, and 
-   # my project makefiles look for them in the GCC objdir. Ergo we'll need 
-   # to build GMP ourselves. We don't need to fully install it; we just need 
-   # its headers.
-   #
-   # First: We need M4, which doesn't come with WSL by default, to configure a 
-   # GMP build.
-   #
+setup_m4() {
    if [ $m4_available != 0 ]; then
       set_window_title "Attempting to install M4 (required for GMP build)..."
       report_step "Attempting to install M4 (required for GMP build)..."
@@ -329,46 +321,106 @@ if [ $fast == 1 ]; then
          report_error_and_exit "Failed to set up M4."
       fi
    fi
+}
+
+pull_gmp_into_build_dir() {
    #
-   # Next, GMP.
+   # Building GMP via its configure script requires M4.
    #
-   if [ -d gmp ]; then
-      #
-      # Test. Sometimes, Bash seems to think the folder exists when it visibly 
-      # does not. Perhaps GCC creates it, deletes it, and we end up checking 
-      # for it in the split-second between the two events?
-      #
-      sleep 0.2
+   setup_m4;
+   
+   find_gmp_directory() {
+      cd $BASEDIR/dl/gcc-$version/
+      local pattern="gmp-"
+      local current=
+      local result=
+      for current in "${pattern}"*; do
+          [ -d "${current}" ] && result="${current}" && break
+      done
+      echo "${result}"
+   }
+   GMPDIR="${BASEDIR}/dl/gcc-${version}/$(find_gmp_directory)"
+   
+   set_window_title "Installing GMP headers..."
+   report_step "Installing GMP headers to <h>$HOME/gcc/build/${version}/gmp</h>..."
+   echo ""
+   
+   mkdir -p $BASEDIR/build/$version/gmp
+   cd $BASEDIR/build/$version/gmp
+   $GMPDIR/configure --prefix=$BASEDIR/build/$version/gmp
+   make
+   #make install # Don't need the rest of GMP; we just want the headers.
+}
+
+build_and_install_gcc() {
+   mkdir -p $BASEDIR/build/$version
+   mkdir -p $BASEDIR/install/$version
+
+   set_window_title "Building GCC ${version}..."
+   report_step "Building GCC ${version} to <h>$HOME/gcc/build/${version}/</h>...";
+   echo "";
+
+   cd $BASEDIR/build/$version/
+   #
+   # Start by configuring the build process. This step will create the makefiles 
+   # that we'll use to actually build GCC. You could think of it as building the 
+   # makefiles.
+   #
+   ../../dl/gcc-$version/configure --prefix=$BASEDIR/install/$version/ $GCC_CONFIG
+   #
+   # Run the makefiles to build GCC.
+   #
+   make -j$(nproc)
+   if [ $? != 0 ]; then
+      report_error_and_exit "Build failed. Aborting installation process."
    fi
-   if [ ! -d gmp ]; then
-      find_gmp_directory() {
-         cd $BASEDIR/dl/gcc-$version/
-         local pattern="gmp-"
-         local current=
-         local result=
-         for current in "${pattern}"*; do
-             [ -d "${current}" ] && result="${current}" && break
-         done
-         echo "${result}"
-      }
-      GMPDIR="${BASEDIR}/dl/gcc-${version}/$(find_gmp_directory)"
-      
-      set_window_title "Installing GMP headers..."
-      report_step "Installing GMP headers to <h>$HOME/gcc/build/${version}/gmp</h>..."
-      echo ""
-      
-      mkdir -p $BASEDIR/build/$version/gmp
-      cd $BASEDIR/build/$version/gmp
-      $GMPDIR/configure --prefix=$BASEDIR/build/$version/gmp
-      make
-      #make install # Don't need the rest of GMP; we just want the headers.
-   else
-      report_step "GMP headers appear to already be present in <h>$HOME/gcc/build/${version}/gmp</h>\; this is unexpected, so please double-check that they're actually there."
-      echo ""
+   #
+   set_window_title "Installing GCC ${version}..."
+   report_step "Installing GCC version ${version} to <h>$HOME/gcc/install/${version}/</h>..."
+   echo ""
+
+   make install
+   if [ $? != 0 ]; then
+      report_error_and_exit "Installation failed. Aborting installation process."
    fi
-   #
-   # As far as I know, this is the only dependency we have to do this for.
-   #
+   
+   if [[ $fast == 1 ]]; then
+      #
+      # When you perform a bootstrapping build of GCC (the default, I believe), 
+      # it will install GMP headers to `objdir/gmp`, i.e. `build/$version/gmp`. 
+      # However, the --disable-bootstrap config option will prevent this from 
+      # happening.
+      #
+      # Compiling a GCC plug-in requires having the GMP headers available, and 
+      # my project makefiles look for them in the GCC objdir. Ergo we'll need 
+      # to build GMP ourselves. We don't need to fully install it; we just need 
+      # its headers.
+      #
+      cd "${BASEDIR}/build/${version}"
+      if [ -d gmp ]; then
+         #
+         # For whatever reason, this check doesn't actually work, like, ever, 
+         # because bash scripting isn't actually determinstic and it should 
+         # be sent back to Hell where it belongs. The most we can do is tell 
+         # the user to fix things manually.
+         #
+         report_step "GMP headers appear to already be present in <h>$HOME/gcc/build/${version}/gmp</h>\; this is unexpected, so please double-check that they're actually there."
+         echo "If the headers aren't there, run this script with --just-fix-gmp."
+         echo ""
+      else
+         pull_gmp_into_build_dir;
+      fi
+      #
+      # As far as I know, this is the only dependency we have to do this for.
+      #
+   fi
+}
+if [[ $just_fix_gmp == 0 ]]; then
+   build_and_install_gcc;
+fi
+
+if [[ $just_fix_gmp != 0 ]]; then
+   pull_gmp_into_build_dir;
 fi
 
 set_window_title "Finished installing GCC ${version}"
