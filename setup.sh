7ca0f8af56db9ac1bc31b0a0aea6de8e777dd305
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2024-12-16 22:56:19 -0500

    Improved shell script for setting up alternate GCC versions
    Fixed variable substitution in some places within the bash script I'm using to install new GCC versions. These worked inconsistently before; they should work consistently now.
    
    Added an option to the bash script to (hopefully) build GCC more quickly. Forty-five-minute build times (under WSL1) are... unpleasant. This option requires some extra machinery (i.e. setting up M4 and running the GMP build process), but it seems to work.
    
    Changed how we highlight text of interest, in our output: instead of calling a formatting function before invoking our printing functions, we now just embed <h></h> in the text to output and have the printing functions replace those. (For some reason, we were getting syntax errors on variable substitution, but only inconsistently. It should work consistently now. I suspect this is going to be a running theme.)

--- setup.sh
+++ setup.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
 BASEDIR=$HOME/gcc
+#
+# The srcdir is $BASEDIR/dl/gcc-$version and the objdir is $BASEDIR/build/$version.
+#
+
+#
+# Options for the GCC build.
+#
+#    --enable-languages
+#       What languages we wish to be able to compile.
+#
+#    --disable-multilib
+#       Only build an x64 GCC that is, itself, only able to build software 
+#       for x64. (If you're in the future, sub out "x64" for whatever CPU 
+#       architecture you use.)
+#
+GCC_CONFIG="--enable-languages=c,c++ --disable-multilib"
 
 usage() {
-   echo "Usage: $0 -v <version-number>";
-   echo "Version numbers should be of the form <int>.<int>.<int>."
+   echo "Usage:";
+   echo " setup.sh [options]"
+   echo ""
+   echo "Downloads and installs a given GCC version using a hardcoded folder hierarchy that "
+   echo "permits multiple versions to live side-by-side. The plug-in project makefiles are "
+   echo "set up to rely on the same folder hierarchy."
+   echo ""
+   echo "Required options::"
+   echo " -v, --version <a>.<b>.<c>       desired GCC version"
+   echo ""
+   echo "Additional options:"
+   echo " -f, --fast                      fast build (w/o localization, bootstrapping, etc.)"
    exit 1;
 }
 
 version=
+fast=0
 extract_args() {
    #
    # Check for GNU's variant on getopt, which properly supports long option 
@@ -19,10 +46,13 @@ extract_args() {
    local vercheck=$(getopt -T)
    local parsed=
    if (( $? != 4)) && [[ -n $vercheck ]]; then
-      while getopts "v:" o; do
+      while getopts "v:f" o; do
          case "${o}" in
             v)
                version=${OPTARG}
+               ;;
+            f)
+               fast=1
                ;;
             *)
                usage
@@ -40,7 +70,7 @@ extract_args() {
       # for the lack of separators and the fact that all names are only 
       # one character long.
       #
-      parsed="$(getopt -a -l 'version:' -o 'v:' -- $@)"
+      parsed="$(getopt -a -l 'version:,fast' -o 'v:f' -- $@)"
       if [ $? != "0" ]; then
          echo "bad"
          usage
@@ -53,6 +83,10 @@ extract_args() {
             "-v"|"--version")
                version="$2"
                shift 2 # consume the key and the value
+               ;;
+            "-f"|"--fast")
+               fast=1
+               shift
                ;;
             # Boilerplate:
             --) # leftover: the argument separator we gave to `getopt`
@@ -73,13 +107,32 @@ if [ -z "${version}" ] ; then
    usage
 fi
 
+pv_available=$(command -v pv > /dev/null)
+
+if [ $fast == 1 ]; then
+   #
+   # --disable-nls
+   #    Don't install localization files.
+   #
+   # --disable-dependency-tracking
+   #    Don't track and cache dependency information that would be useless 
+   #    for one-off builds of a program that I don't intend to modify.
+   #
+   # --disable-bootstrap
+   #    Use a simpler build process, because we don't care whether the GCC 
+   #    we're building here can build other GCCs.
+   #
+   # On my machine (WSL1 on an SSD), this cuts GCC's build time down from 
+   # 45 minutes to about 11 minutes. This doesn't include the time it takes 
+   # for us to build GMP into the folder our plug-in project expects it to 
+   # be in, though.
+   #
+   GCC_CONFIG="${GCC_CONFIG} --disable-nls --disable-dependency-tracking --disable-bootstrap"
+fi
+
+formatting_for_highlight="$(printf '\x1b[38;2;0;255;255m')"
 formatting_for_error="$(printf '\x1b[0;38;2;255;64;64m')"
 formatting_for_step="$(printf '\x1b[0;1m')"
-
-# Usage: print_step "Some info: $(highlight_token foo)"
-highlight_token() {
-   printf "\x1b[38;2;0;255;255m$1"
-}
 
 report_step() {
    printf "\n";
@@ -90,7 +143,11 @@ report_step() {
    # Print an "i" icon
    printf "\x1b[38;${white}m\u2590\x1b[38;${blue};48;${white}mi\x1b[0;38;${white}m\u258C\x1b[0m"
    
-   printf "${formatting_for_step}%s\x1b[0m\n" "$1";
+   local text="$1"
+   text="${text//<h>/$formatting_for_highlight}"
+   text="${text//<\/h>/$formatting_for_step}"
+   
+   printf "${formatting_for_step}%s\x1b[0m\n" "${text}";
    printf "\n";
 }
 
@@ -103,64 +160,156 @@ report_error_and_exit() {
    # Print an "!" icon
    printf "\x1b[38;${red}m\u2590\x1b[1;38;${white};48;${red}m!\x1b[0;38;${red}m\u258C\x1b[0m"
    
-   printf "\x1b[38;${red}m%s\x1b[0m\n" "$1";
+   local text="$1"
+   text="${text//<h>/$formatting_for_highlight}"
+   text="${text//<\/h>/$formatting_for_step}"
+   
+   printf "\x1b[38;${red}m%s\x1b[0m\n" "${text}";
    printf "\n";
    exit 1;
 }
 
 cd $BASEDIR/dl
 if [ -f "gcc-${version}.tar.gz" ]; then
-   report_step "You already have the archive for GCC ${version} downloaded to $(highlight_token $HOME/gcc/dl/gcc-${version}.tar.gz)${formatting_for_step}; skipping download."
+   report_step "You already have the archive for GCC ${version} downloaded to <h>$HOME/gcc/dl/gcc-${version}.tar.gz</h>; skipping download."
 else
-   report_step "Downloading GCC $(version) to $(highlight_token $HOME/gcc/dl)${formatting_for_step}..."
+   report_step "Downloading GCC ${version} to <h>$HOME/gcc/dl/</h>..."
+   echo "Downloading from: http://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz"
+   echo "";
 
-   wget http://ftp.gnu.org/gnu/gcc/gcc-$(version)/gcc-$(version).tar.gz
+   wget "http://ftp.gnu.org/gnu/gcc/gcc-${version}/gcc-${version}.tar.gz" --no-clobber --continue
    result=$?
-   if $result -eq 1; then
+   if [ $result == 1 ]; then
       report_error_and_exit "Download failed. Aborting installation process."
-   elif $result -eq 3; then
+   elif [ $result == 3 ]; then
       report_error_and_exit "Download failed. (File I/O issue.) Aborting installation process."
-   elif $result -eq 4; then
+   elif [ $result == 4 ]; then
       report_error_and_exit "Download failed. (Network failure.) Aborting installation process."
-   elif $result -eq 5; then
+   elif [ $result == 5 ]; then
       report_error_and_exit "Download failed. (SSL verification failure.) Aborting installation process."
-   elif $result -eq 8; then
+   elif [ $result == 8 ]; then
       report_error_and_exit "Download failed. (Server issued an error response; missing file?) Aborting installation process."
-   elif $result -neq 0; then
+   elif [ $result != 0 ]; then
       report_error_and_exit "Download failed. Aborting installation process."
    fi
 fi
 #
-report_step "Unpacking GCC ${version} to $(highlight_token $HOME/gcc/dl/gcc-${version}/)${formatting_for_step}..."
-tar xf "gcc-${version}.tar.gz" --skip-old-files
-if [ $? != 0 ]; then
+report_step "Unpacking GCC ${version} to <h>$HOME/gcc/dl/gcc-${version}/</h>..."
+
+result=0
+if [ ! $pv_available ]; then
+   #
+   # The `pv` command is not installed. Extract files without a 
+   # progress indicator.
+   #
+   echo ""
+   
+   tar xf "gcc-${version}.tar.gz" --skip-old-files --checkpoint-action=ttyout='(%d seconds elapsed): %T%*\r'
+   result=$?
+else
+   #
+   # If the `pv` command is available, then use it to show a 
+   # progress bar for the extraction. In this case, `pv` is 
+   # able to show progress because it's responsible for feeding 
+   # the input file into `tar`, and it knows how much it's fed.
+   #
+   echo ""
+   
+   pv "gcc-${version}.tar.gz" | tar x --skip-old-files
+   result=${PIPESTATUS[1]}
+fi
+if [ $result != 0 ]; then
    report_error_and_exit "Unpacking failed. Aborting installation process."
 fi
 
 report_step "Downloading prerequisites (in-tree) for GCC ${version}..."
+echo ""
+
 cd "gcc-${version}"
 ./contrib/download_prerequisites
 
-cd $BASEDIR/build
-mkdir ${version}
-cd ..
-cd install
-mkdir ${version}
-cd ..
+mkdir -p $BASEDIR/build/$version
+mkdir -p $BASEDIR/install/$version
 
-report_step "Building GCC version ${version} to $(highlight_token $HOME/gcc/build/${version}/)${formatting_for_step}..."
+report_step "Building GCC version ${version} to <h>$HOME/gcc/build/${version}/</h>...";
+echo "";
+
 cd build/$version/
-../../dl/gcc-$version/configure --prefix=$HOME/gcc/install/$version/ --enable-languages=c,c++ --disable-multilib
-
+#
+# Start by configuring the build process. This step will create the makefiles 
+# that we'll use to actually build GCC. You could think of it as building the 
+# makefiles.
+#
+../../dl/gcc-$version/configure --prefix=$BASEDIR/install/$version/ $GCC_CONFIG
+#
+# Run the makefiles to build GCC.
+#
 make -j$(nproc)
 if [ $? != 0 ]; then
    report_error_and_exit "Build failed. Aborting installation process."
 fi
+#
+report_step "Installing GCC version ${version} to <h>$HOME/gcc/install/${version}/</h>..."
+echo ""
 
-report_step "Installing GCC version ${version} to $(highlight_token $HOME/gcc/install/${version}/)${formatting_for_step}..."
 make install
 if [ $? != 0 ]; then
    report_error_and_exit "Installation failed. Aborting installation process."
 fi
 
+if [ $fast == 1 ]; then
+   #
+   # When you perform a bootstrapping build of GCC (the default, I believe), 
+   # it will install GMP headers to `objdir/gmp`, i.e. `build/$version/gmp`. 
+   # However, the --disable-bootstrap config option will prevent this from 
+   # happening.
+   #
+   # Compiling a GCC plug-in requires having the GMP headers available, and 
+   # my project makefiles look for them in the GCC objdir. Ergo we'll need 
+   # to build GMP ourselves. We don't need to fully install it; we just need 
+   # its headers.
+   #
+   # First: We need M4, which doesn't come with WSL by default, to configure a 
+   # GMP build.
+   #
+   command -v m4
+   if [ $? != 0 ]; then
+      report_step "Attempting to install M4 (required for GMP build)..."
+      echo ""
+      sudo apt-get install -y m4
+      if [ $? != 0 ]; then
+         report_error_and_exit "Failed to set up M4."
+      fi
+   fi
+   #
+   # Next, GMP.
+   #
+   if [ ! -d gmp ]; then
+      find_gmp_directory() {
+         cd $BASEDIR/dl/gcc-$version/
+         local pattern="gmp-"
+         local current=
+         local result=
+         for current in "${pattern}"*; do
+             [ -d "${current}" ] && result="${current}" && break
+         done
+         echo "${result}"
+      }
+      GMPDIR="${BASEDIR}/dl/gcc-${version}/$(find_gmp_directory)"
+      
+      report_step "Installing GMP headers to <h>$HOME/gcc/build/${version}/gmp</h>..."
+      echo ""
+      
+      mkdir -p $BASEDIR/build/$version/gmp
+      cd $BASEDIR/build/$version/gmp
+      $GMPDIR/configure --prefix=$BASEDIR/build/$version/gmp
+      make
+      #make install # Don't need the rest of GMP; we just want the headers.
+   fi
+   #
+   # As far as I know, this is the only dependency we have to do this for.
+   #
+fi
+
 report_step "Finished installing GCC version ${version}."
+printf "\n";
